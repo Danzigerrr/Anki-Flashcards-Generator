4c7212cfa923e38c784bcae31942634b754ddc16
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-10-16 22:12:26 +0200

    import flashcards_creator methods to main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import genanki
-from csv_reader import *
-
+from csv_reader import read_csv_file
+from flashcards_creator import create_flashcards
 
 if __name__ == '__main__':
     questions_and_answers = read_csv_file('data.csv')
-
+    create_flashcards(questions_and_answers)
 
