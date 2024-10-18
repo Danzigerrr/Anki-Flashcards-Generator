a62ba548ce6f135802a5b299945477bb6cfb9e7c
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-10-18 09:52:03 +0200

    refactor create_flashcards() method

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from flashcards_creator import create_flashcards
 if __name__ == '__main__':
     csv_filename = 'data.csv'
     csv_delimiter = ','
-
     questions_and_answers = read_csv_file(csv_filename, csv_delimiter)
-    create_flashcards(questions_and_answers)
+
+    deck_name = "English to German words"
+    create_flashcards(questions_and_answers, deck_name)
 
