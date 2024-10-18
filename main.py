d86f8033cc3dc6dc689c9ae8fe1245b8559dc7d2
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-10-18 09:50:40 +0200

    add csv filename variable

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from csv_reader import read_csv_file
 from flashcards_creator import create_flashcards
 
 if __name__ == '__main__':
+    csv_filename = 'data.csv'
     csv_delimiter = ','
 
-    questions_and_answers = read_csv_file('data.csv', csv_delimiter)
+    questions_and_answers = read_csv_file(csv_filename, csv_delimiter)
     create_flashcards(questions_and_answers)
 
